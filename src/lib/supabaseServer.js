b4b971e4713d81820a1d89b259fc7b7dd0891ce8
Albert Alston <albertalston@Alberts-MBP-2.lan>
2026-01-25 21:03:10 -0500

    Fix supabase server cookies (t.get error)

--- src/lib/supabaseServer.js
+++ src/lib/supabaseServer.js
@@ -1,3 +1,4 @@
+// src/lib/supabaseServer.js
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
@@ -13,10 +14,10 @@ export function supabaseServer() {
           return cookieStore.get(name)?.value;
         },
         set(name, value, options) {
-          cookieStore.set(name, value, options);
+          cookieStore.set({ name, value, ...options });
         },
         remove(name, options) {
-          cookieStore.set(name, "", { ...options, maxAge: 0 });
+          cookieStore.set({ name, value: "", ...options, maxAge: 0 });
         },
       },
     }
